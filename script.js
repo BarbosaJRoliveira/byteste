2a4d2247df1082066ee24fc41bfd14d38dc00665
BarbosaJRoliveira <juniorbarbosa100@outlook.com>
2023-11-24 08:44:10 -0300

    jjj
    ggg

---
+++ script.js
@@ -0,0 +1,55 @@
+let nome      
+var valor1
+var valor2
+var resultado1
+var resultado2
+var resultado3
+var resultado4
+
+
+nome=prompt("Qual é o seu nome?")// aqui e o escreva
+valor1=prompt("Qual e o numero 1?")
+valor2=prompt("Qual e o numero 2?")
+
+resultado1=parseFloat(valor1)+parseFloat(valor2)
+alert("o resultado: "+ resultado1)
+
+resultado2=parseFloat(valor1)*parseFloat(valor2)
+alert("o resultado: "+ resultado2)
+
+resultado3=parseFloat(valor1)-parseFloat(valor2)
+alert("o resultado: "+ resultado3)
+
+resultado4=parseFloat(valor1)/parseFloat(valor2)
+alert("o resultado: "+ resultado4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
